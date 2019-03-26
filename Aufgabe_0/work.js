75f17697f6352e27e769b6a3ffb6b989df70b8c9
HannesSchwabe <hannes.schwabe@hs-furtwangen.de>
2019-03-26 16:04:41 +0100

    update verbesserung

--- Aufgabe_0/work.js
+++ Aufgabe_0/work.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", namenAusgabe);
 function namenAusgabe() {
-    var promptValue = prompt('Dein Name:');
+    let promptValue = prompt('Dein Name:');
     console.log('Hallo ' + promptValue + ' !');
     document.getElementById('name').innerHTML = 'Hallo ' + promptValue + '!';
 }
