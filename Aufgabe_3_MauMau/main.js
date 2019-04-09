f0a67a497a1f72b5c435a55d44e5827e45a69ed7
HannesSchwabe <hannes.schwabe@hs-furtwangen.de>
2019-04-09 17:10:36 +0200

    verbesserung maumau code aufgabe 3

--- Aufgabe_3_MauMau/main.js
+++ Aufgabe_3_MauMau/main.js
@@ -1,216 +1,218 @@
-let karte1 = {
-    zahl: "7",
-    rot: false,
-    symbol: "pik",
-};
-let karte2 = {
-    zahl: "8",
-    rot: false,
-    symbol: "pik",
-};
-let karte3 = {
-    zahl: "9",
-    rot: false,
-    symbol: "pik",
-};
-let karte4 = {
-    zahl: "10",
-    rot: false,
-    symbol: "pik",
-};
-let karte5 = {
-    zahl: "bube",
-    rot: false,
-    symbol: "pik",
-};
-let karte6 = {
-    zahl: "dame",
-    rot: false,
-    symbol: "pik",
-};
-let karte7 = {
-    zahl: "koenig",
-    rot: false,
-    symbol: "pik",
-};
-let karte8 = {
-    zahl: "ass",
-    rot: false,
-    symbol: "pik",
-};
-let karte9 = {
-    zahl: "7",
-    rot: false,
-    symbol: "kreuz",
-};
-let karte10 = {
-    zahl: "8",
-    rot: false,
-    symbol: "kreuz",
-};
-let karte11 = {
-    zahl: "9",
-    rot: false,
-    symbol: "kreuz",
-};
-let karte12 = {
-    zahl: "10",
-    rot: false,
-    symbol: "kreuz",
-};
-let karte13 = {
-    zahl: "bube",
-    rot: false,
-    symbol: "kreuz",
-};
-let karte14 = {
-    zahl: "dame",
-    rot: false,
-    symbol: "kreuz",
-};
-let karte15 = {
-    zahl: "koenig",
-    rot: false,
-    symbol: "kreuz",
-};
-let karte16 = {
-    zahl: "ass",
-    rot: false,
-    symbol: "kreuz",
-};
-let karte17 = {
-    zahl: "7",
-    rot: true,
-    symbol: "herz",
-};
-let karte18 = {
-    zahl: "8",
-    rot: true,
-    symbol: "herz",
-};
-let karte19 = {
-    zahl: "9",
-    rot: true,
-    symbol: "herz",
-};
-let karte20 = {
-    zahl: "10",
-    rot: true,
-    symbol: "herz",
-};
-let karte21 = {
-    zahl: "bube",
-    rot: true,
-    symbol: "herz",
-};
-let karte22 = {
-    zahl: "dame",
-    rot: true,
-    symbol: "herz",
-};
-let karte23 = {
-    zahl: "koenig",
-    rot: true,
-    symbol: "herz",
-};
-let karte24 = {
-    zahl: "ass",
-    rot: true,
-    symbol: "herz",
-};
-let karte25 = {
-    zahl: "7",
-    rot: true,
-    symbol: "herz",
-};
-let karte26 = {
-    zahl: "8",
-    rot: true,
-    symbol: "herz",
-};
-let karte27 = {
-    zahl: "9",
-    rot: true,
-    symbol: "herz",
-};
-let karte28 = {
-    zahl: "10",
-    rot: true,
-    symbol: "herz",
-};
-let karte29 = {
-    zahl: "bube",
-    rot: true,
-    symbol: "herz",
-};
-let karte30 = {
-    zahl: "dame",
-    rot: true,
-    symbol: "herz",
-};
-let karte31 = {
-    zahl: "koenig",
-    rot: true,
-    symbol: "herz",
-};
-let karte32 = {
-    zahl: "ass",
-    rot: true,
-    symbol: "herz",
-};
-let ziehstapel = [karte1, karte2, karte3, karte4, karte5, karte6, karte7, karte8, karte9, karte10, karte11, karte12, karte13, karte14, karte15, karte16, karte17, karte18, karte19, karte20, karte21, karte22, karte23, karte24, karte25, karte26, karte27, karte28, karte29, karte30, karte31, karte32];
-let spielstapel = [];
-let handstapel = [];
-document.addEventListener("DOMContentLoaded", anzahlHandkarten);
-function anzahlHandkarten() {
-    let base = 10;
-    let promptValue = prompt('Wie viele Karten?');
-    let anzahl = parseInt(promptValue, base);
-    pushekartenInHandstapel(anzahl);
-}
-function pushekartenInHandstapel(_anzahl) {
-    let k = 0;
-    for (let i = 0; i < _anzahl; i++) {
-        k = Math.floor(Math.random() * ziehstapel.length);
-        handstapel.push(ziehstapel[k]);
-        let removed = ziehstapel.splice(k, 1);
-        placeHandstapel(handstapel[i]);
+var MauMau;
+(function (MauMau) {
+    let karte1 = {
+        zahl: "7",
+        rot: false,
+        symbol: "pik",
+    };
+    let karte2 = {
+        zahl: "8",
+        rot: false,
+        symbol: "pik",
+    };
+    let karte3 = {
+        zahl: "9",
+        rot: false,
+        symbol: "pik",
+    };
+    let karte4 = {
+        zahl: "10",
+        rot: false,
+        symbol: "pik",
+    };
+    let karte5 = {
+        zahl: "bube",
+        rot: false,
+        symbol: "pik",
+    };
+    let karte6 = {
+        zahl: "dame",
+        rot: false,
+        symbol: "pik",
+    };
+    let karte7 = {
+        zahl: "koenig",
+        rot: false,
+        symbol: "pik",
+    };
+    let karte8 = {
+        zahl: "ass",
+        rot: false,
+        symbol: "pik",
+    };
+    let karte9 = {
+        zahl: "7",
+        rot: false,
+        symbol: "kreuz",
+    };
+    let karte10 = {
+        zahl: "8",
+        rot: false,
+        symbol: "kreuz",
+    };
+    let karte11 = {
+        zahl: "9",
+        rot: false,
+        symbol: "kreuz",
+    };
+    let karte12 = {
+        zahl: "10",
+        rot: false,
+        symbol: "kreuz",
+    };
+    let karte13 = {
+        zahl: "bube",
+        rot: false,
+        symbol: "kreuz",
+    };
+    let karte14 = {
+        zahl: "dame",
+        rot: false,
+        symbol: "kreuz",
+    };
+    let karte15 = {
+        zahl: "koenig",
+        rot: false,
+        symbol: "kreuz",
+    };
+    let karte16 = {
+        zahl: "ass",
+        rot: false,
+        symbol: "kreuz",
+    };
+    let karte17 = {
+        zahl: "7",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte18 = {
+        zahl: "8",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte19 = {
+        zahl: "9",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte20 = {
+        zahl: "10",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte21 = {
+        zahl: "bube",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte22 = {
+        zahl: "dame",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte23 = {
+        zahl: "koenig",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte24 = {
+        zahl: "ass",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte25 = {
+        zahl: "7",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte26 = {
+        zahl: "8",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte27 = {
+        zahl: "9",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte28 = {
+        zahl: "10",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte29 = {
+        zahl: "bube",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte30 = {
+        zahl: "dame",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte31 = {
+        zahl: "koenig",
+        rot: true,
+        symbol: "herz",
+    };
+    let karte32 = {
+        zahl: "ass",
+        rot: true,
+        symbol: "herz",
+    };
+    let ziehstapel = [karte1, karte2, karte3, karte4, karte5, karte6, karte7, karte8, karte9, karte10, karte11, karte12, karte13, karte14, karte15, karte16, karte17, karte18, karte19, karte20, karte21, karte22, karte23, karte24, karte25, karte26, karte27, karte28, karte29, karte30, karte31, karte32];
+    let spielstapel = [];
+    let handstapel = [];
+    document.addEventListener("DOMContentLoaded", anzahlHandkarten);
+    function anzahlHandkarten() {
+        let base = 10;
+        let promptValue = prompt('Wie viele Karten?');
+        let anzahl = parseInt(promptValue, base);
+        pushekartenInHandstapel(anzahl);
     }
-    pushekartenInSpielstapel();
-    for (let i = 0; i < 32; i++) {
-        placeZiehstapel(ziehstapel[i]);
+    function pushekartenInHandstapel(_anzahl) {
+        let k = 0;
+        for (let i = 0; i < _anzahl; i++) {
+            k = Math.floor(Math.random() * ziehstapel.length);
+            handstapel.push(ziehstapel[k]);
+            let removed = ziehstapel.splice(k, 1);
+            placeHandstapel(handstapel[i]);
+        }
+        pushekartenInSpielstapel();
+        for (let i = 0; i < 32; i++) {
+            placeZiehstapel(ziehstapel[i]);
+        }
     }
-}
-function pushekartenInSpielstapel() {
-    for (let i = 0; i < 1; i++) {
+    function pushekartenInSpielstapel() {
+        let i = 0;
         let k = Math.floor(Math.random() * ziehstapel.length);
         spielstapel.push(ziehstapel[k]);
         let removed = ziehstapel.splice(k, 1);
         placeSpielstapel(spielstapel[i]);
     }
-}
-function placeHandstapel(k) {
-    let prodElement = document.createElement('div');
-    prodElement.innerHTML = `<div class="handstapel">
+    function placeHandstapel(k) {
+        let prodElement = document.createElement('div');
+        prodElement.innerHTML = `<div class="handstapel">
     <p> ${k.zahl} </p>
     <p> ${k.symbol} </p>
     </div>`;
-    document.getElementById("body").appendChild(prodElement);
-}
-function placeZiehstapel(k) {
-    let prodElement = document.createElement('div');
-    prodElement.innerHTML = `<div class="ziehstapel">
+        document.getElementById("body").appendChild(prodElement);
+    }
+    function placeZiehstapel(k) {
+        let prodElement = document.createElement('div');
+        prodElement.innerHTML = `<div class="ziehstapel">
     <p> ${k.zahl} </p>
     <p> ${k.symbol} </p>
     </div>`;
-    document.getElementById("body").appendChild(prodElement);
-}
-function placeSpielstapel(_k) {
-    let prodElement = document.createElement('div');
-    prodElement.innerHTML = `<div class="spielstapel">
+        document.getElementById("body").appendChild(prodElement);
+    }
+    function placeSpielstapel(_k) {
+        let prodElement = document.createElement('div');
+        prodElement.innerHTML = `<div class="spielstapel">
     <p> ${_k.zahl} </p>
     <p> ${_k.symbol} </p>
     </div>`;
-    document.getElementById("body").appendChild(prodElement);
-}
+        document.getElementById("body").appendChild(prodElement);
+    }
+})(MauMau || (MauMau = {}));
 //# sourceMappingURL=main.js.map
--- Aufgabe_3_MauMau/main.ts
+++ Aufgabe_3_MauMau/main.ts
@@ -1,259 +1,265 @@
-interface spielkarte {
-    zahl: string;
-    rot: boolean;
-    symbol: string;
-}
+namespace MauMau {
 
-let karte1: spielkarte = {
-    zahl: "7",
-    rot: false,
-    symbol: "pik",
-}
 
-let karte2: spielkarte = {
-    zahl: "8",
-    rot: false,
-    symbol: "pik",
-}
 
-let karte3: spielkarte = {
-    zahl: "9",
-    rot: false,
-    symbol: "pik",
-}
-
-let karte4: spielkarte = {
-    zahl: "10",
-    rot: false,
-    symbol: "pik",
-}
-
-let karte5: spielkarte = {
-    zahl: "bube",
-    rot: false,
-    symbol: "pik",
-}
-
-let karte6: spielkarte = {
-    zahl: "dame",
-    rot: false,
-    symbol: "pik",
-}
-
-let karte7: spielkarte = {
-    zahl: "koenig",
-    rot: false,
-    symbol: "pik",
-}
-
-let karte8: spielkarte = {
-    zahl: "ass",
-    rot: false,
-    symbol: "pik",
-}
-
-let karte9: spielkarte = {
-    zahl: "7",
-    rot: false,
-    symbol: "kreuz",
-}
-
-let karte10: spielkarte = {
-    zahl: "8",
-    rot: false,
-    symbol: "kreuz",
-}
-
-let karte11: spielkarte = {
-    zahl: "9",
-    rot: false,
-    symbol: "kreuz",
-}
-
-let karte12: spielkarte = {
-    zahl: "10",
-    rot: false,
-    symbol: "kreuz",
-}
-
-let karte13: spielkarte = {
-    zahl: "bube",
-    rot: false,
-    symbol: "kreuz",
-}
-
-let karte14: spielkarte = {
-    zahl: "dame",
-    rot: false,
-    symbol: "kreuz",
-}
-
-let karte15: spielkarte = {
-    zahl: "koenig",
-    rot: false,
-    symbol: "kreuz",
-}
-
-let karte16: spielkarte = {
-    zahl: "ass",
-    rot: false,
-    symbol: "kreuz",
-}
-
-let karte17: spielkarte = {
-    zahl: "7",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte18: spielkarte = {
-    zahl: "8",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte19: spielkarte = {
-    zahl: "9",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte20: spielkarte = {
-    zahl: "10",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte21: spielkarte = {
-    zahl: "bube",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte22: spielkarte = {
-    zahl: "dame",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte23: spielkarte = {
-    zahl: "koenig",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte24: spielkarte = {
-    zahl: "ass",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte25: spielkarte = {
-    zahl: "7",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte26: spielkarte = {
-    zahl: "8",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte27: spielkarte = {
-    zahl: "9",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte28: spielkarte = {
-    zahl: "10",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte29: spielkarte = {
-    zahl: "bube",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte30: spielkarte = {
-    zahl: "dame",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte31: spielkarte = {
-    zahl: "koenig",
-    rot: true,
-    symbol: "herz",
-}
-
-let karte32: spielkarte = {
-    zahl: "ass",
-    rot: true,
-    symbol: "herz",
-}
-
-let ziehstapel: spielkarte[] = [karte1, karte2, karte3, karte4, karte5, karte6, karte7, karte8, karte9, karte10, karte11, karte12, karte13, karte14, karte15, karte16, karte17, karte18, karte19, karte20, karte21, karte22, karte23, karte24, karte25, karte26, karte27, karte28, karte29, karte30, karte31, karte32];
-let spielstapel: spielkarte[] = [];
-let handstapel: spielkarte[] = [];
-
-document.addEventListener("DOMContentLoaded", anzahlHandkarten);
-function anzahlHandkarten(): void {
-    let base = 10;
-    let promptValue: string = prompt('Wie viele Karten?');
-    let anzahl = parseInt(promptValue, base);
-    pushekartenInHandstapel(anzahl);
-}
-
-function pushekartenInHandstapel(_anzahl: number): void {
-    let k: number = 0;
-    for (let i: number = 0; i < _anzahl; i++) {
-        k = Math.floor(Math.random() * ziehstapel.length);
-        handstapel.push(ziehstapel[k]);
-        let removed = ziehstapel.splice(k, 1);
-        placeHandstapel(handstapel[i]);
+    interface spielkarte {
+        zahl: string;
+        rot: boolean;
+        symbol: string;
     }
-    pushekartenInSpielstapel();
-    for(let i: number = 0; i < 32; i++) {
-        placeZiehstapel(ziehstapel[i]);
-    }
-}
 
-function pushekartenInSpielstapel(): void {
-    for(let i: number = 0; i < 1; i++) {
+    let karte1: spielkarte = {
+        zahl: "7",
+        rot: false,
+        symbol: "pik",
+    }
+
+    let karte2: spielkarte = {
+        zahl: "8",
+        rot: false,
+        symbol: "pik",
+    }
+
+    let karte3: spielkarte = {
+        zahl: "9",
+        rot: false,
+        symbol: "pik",
+    }
+
+    let karte4: spielkarte = {
+        zahl: "10",
+        rot: false,
+        symbol: "pik",
+    }
+
+    let karte5: spielkarte = {
+        zahl: "bube",
+        rot: false,
+        symbol: "pik",
+    }
+
+    let karte6: spielkarte = {
+        zahl: "dame",
+        rot: false,
+        symbol: "pik",
+    }
+
+    let karte7: spielkarte = {
+        zahl: "koenig",
+        rot: false,
+        symbol: "pik",
+    }
+
+    let karte8: spielkarte = {
+        zahl: "ass",
+        rot: false,
+        symbol: "pik",
+    }
+
+    let karte9: spielkarte = {
+        zahl: "7",
+        rot: false,
+        symbol: "kreuz",
+    }
+
+    let karte10: spielkarte = {
+        zahl: "8",
+        rot: false,
+        symbol: "kreuz",
+    }
+
+    let karte11: spielkarte = {
+        zahl: "9",
+        rot: false,
+        symbol: "kreuz",
+    }
+
+    let karte12: spielkarte = {
+        zahl: "10",
+        rot: false,
+        symbol: "kreuz",
+    }
+
+    let karte13: spielkarte = {
+        zahl: "bube",
+        rot: false,
+        symbol: "kreuz",
+    }
+
+    let karte14: spielkarte = {
+        zahl: "dame",
+        rot: false,
+        symbol: "kreuz",
+    }
+
+    let karte15: spielkarte = {
+        zahl: "koenig",
+        rot: false,
+        symbol: "kreuz",
+    }
+
+    let karte16: spielkarte = {
+        zahl: "ass",
+        rot: false,
+        symbol: "kreuz",
+    }
+
+    let karte17: spielkarte = {
+        zahl: "7",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte18: spielkarte = {
+        zahl: "8",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte19: spielkarte = {
+        zahl: "9",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte20: spielkarte = {
+        zahl: "10",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte21: spielkarte = {
+        zahl: "bube",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte22: spielkarte = {
+        zahl: "dame",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte23: spielkarte = {
+        zahl: "koenig",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte24: spielkarte = {
+        zahl: "ass",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte25: spielkarte = {
+        zahl: "7",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte26: spielkarte = {
+        zahl: "8",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte27: spielkarte = {
+        zahl: "9",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte28: spielkarte = {
+        zahl: "10",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte29: spielkarte = {
+        zahl: "bube",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte30: spielkarte = {
+        zahl: "dame",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte31: spielkarte = {
+        zahl: "koenig",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let karte32: spielkarte = {
+        zahl: "ass",
+        rot: true,
+        symbol: "herz",
+    }
+
+    let ziehstapel: spielkarte[] = [karte1, karte2, karte3, karte4, karte5, karte6, karte7, karte8, karte9, karte10, karte11, karte12, karte13, karte14, karte15, karte16, karte17, karte18, karte19, karte20, karte21, karte22, karte23, karte24, karte25, karte26, karte27, karte28, karte29, karte30, karte31, karte32];
+    let spielstapel: spielkarte[] = [];
+    let handstapel: spielkarte[] = [];
+
+    document.addEventListener("DOMContentLoaded", anzahlHandkarten);
+    function anzahlHandkarten(): void {
+        let base = 10;
+        let promptValue: string = prompt('Wie viele Karten?');
+        let anzahl = parseInt(promptValue, base);
+        pushekartenInHandstapel(anzahl);
+    }
+
+    function pushekartenInHandstapel(_anzahl: number): void {
+        let k: number = 0;
+        for (let i: number = 0; i < _anzahl; i++) {
+            k = Math.floor(Math.random() * ziehstapel.length);
+            handstapel.push(ziehstapel[k]);
+            let removed = ziehstapel.splice(k, 1);
+            placeHandstapel(handstapel[i]);
+        }
+        pushekartenInSpielstapel();
+        for (let i: number = 0; i < 32; i++) {
+            placeZiehstapel(ziehstapel[i]);
+        }
+    }
+
+    function pushekartenInSpielstapel(): void {
+        let i: number = 0;
         let k: number = Math.floor(Math.random() * ziehstapel.length);
         spielstapel.push(ziehstapel[k]);
         let removed = ziehstapel.splice(k, 1);
         placeSpielstapel(spielstapel[i]);
+
     }
-}
 
-function placeHandstapel(k: spielkarte): void {
-    let prodElement = document.createElement('div');
-    prodElement.innerHTML = `<div class="handstapel">
+    function placeHandstapel(k: spielkarte): void {
+        let prodElement = document.createElement('div');
+        prodElement.innerHTML = `<div class="handstapel">
     <p> ${k.zahl} </p>
     <p> ${k.symbol} </p>
     </div>`
-    document.getElementById("body").appendChild(prodElement);
-}
+        document.getElementById("body").appendChild(prodElement);
+    }
 
-function placeZiehstapel(k: spielkarte) {
-    let prodElement = document.createElement('div');
-    prodElement.innerHTML = `<div class="ziehstapel">
+    function placeZiehstapel(k: spielkarte) {
+        let prodElement = document.createElement('div');
+        prodElement.innerHTML = `<div class="ziehstapel">
     <p> ${k.zahl} </p>
     <p> ${k.symbol} </p>
     </div>`
-    document.getElementById("body").appendChild(prodElement);
-}
+        document.getElementById("body").appendChild(prodElement);
+    }
 
-function placeSpielstapel(_k: spielkarte) {
-    let prodElement = document.createElement('div');
-    prodElement.innerHTML = `<div class="spielstapel">
+    function placeSpielstapel(_k: spielkarte) {
+        let prodElement = document.createElement('div');
+        prodElement.innerHTML = `<div class="spielstapel">
     <p> ${_k.zahl} </p>
     <p> ${_k.symbol} </p>
     </div>`
-    document.getElementById("body").appendChild(prodElement);
+        document.getElementById("body").appendChild(prodElement);
+    }
+
 }
