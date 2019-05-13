1b979e8a0f29f5dd1304fe57abc0ab955cd36f18
HannesSchwabe <hannes.schwabe@hs-furtwangen.de>
2019-05-13 12:50:47 +0200

    update verbesserung

--- Aufgabe_7_ServerResponse/sendData.js
+++ Aufgabe_7_ServerResponse/sendData.js
@@ -1,31 +1,51 @@
 var L07_SendData;
 (function (L07_SendData) {
     window.addEventListener("load", init);
-    //let address: string = "http://localhost:8100";
-    let address = "https://schwabeh.herokuapp.com?";
+    //let address: string = "http://localhost:8100/";
+    let address = "https://schwabeh.herokuapp.com/";
     let input = document.getElementsByTagName("input");
     function init(_event) {
         let button = document.getElementById("buy");
         button.addEventListener("click", handleClickOnButton);
     }
     function handleClickOnButton(_event) {
+        let link = "?";
         for (let i = 0; i < input.length; i++) {
-            if (input[i].checked == true) {
-                address += input[i].className;
+            if (input[i].checked == true && input[i].name != "Zustellung") {
+                link += input[i].className + " " + "Ja" + "&";
             }
-            else if (input[i].value > "0") {
-                address += input[i].className += input[i].value;
+            if (input[i].value > "0" && input[i].type != "text" && input[i].name != "Zustellung") {
+                link += input[i].name + "=" + " " + input[i].value + "&";
+            }
+            if (input[i].value != "" && input[i].type == "text" && input[i].name != "Zustellung") {
+                link += input[i].name + "=" + " " + input[i].value + "&";
+            }
+            if (input[i].id == "radio3" && input[i].checked == true) {
+                link += input[i].name + "=" + " " + "Lieferung" + "&";
+            }
+            if (input[i].id == "radio4" && input[i].checked == true) {
+                link += input[i].name + "=" + " " + "Abholung" + "&";
             }
         }
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", address, true);
+        xhr.open("GET", address + link, true);
         xhr.addEventListener("readystatechange", handleStateChange);
         xhr.send();
     }
     function handleStateChange(_event) {
         let xhr = _event.target;
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            document.getElementById("anhang").innerHTML = xhr.response;
+            let prodElement = document.createElement('div');
+            prodElement.innerHTML = `<div id="anhang">
+            </div>`;
+            document.getElementById("body").appendChild(prodElement);
+            let bestellungen = document.createElement("p");
+            let uberschrift = document.createElement("h2");
+            uberschrift.innerHTML = "Wir haben Ihre Bestellung erhalten!";
+            document.getElementById("anhang").appendChild(uberschrift);
+            bestellungen.innerHTML = `${xhr.response}`;
+            document.getElementById("anhang").appendChild(bestellungen);
+            console.log(xhr.response);
         }
     }
 })(L07_SendData || (L07_SendData = {}));
--- Aufgabe_7_ServerResponse/sendData.ts
+++ Aufgabe_7_ServerResponse/sendData.ts
@@ -1,7 +1,7 @@
 namespace L07_SendData {
     window.addEventListener("load", init);
-    //let address: string = "http://localhost:8100";
-    let address: string = "https://schwabeh.herokuapp.com?";
+    //let address: string = "http://localhost:8100/";
+    let address: string = "https://schwabeh.herokuapp.com/";
     let input: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
 
     function init(_event: Event): void {
@@ -11,17 +11,31 @@ namespace L07_SendData {
 
 
     function handleClickOnButton(_event: Event): void {
+        let link: string = "?";
         for (let i: number = 0; i < input.length; i++) {
-            if (input[i].checked == true) {
-                address += input[i].className;  
+            if (input[i].checked == true && input[i].name != "Zustellung") {
+                link += input[i].className + " " + "Ja" + "&";
             }
 
-            else if (input[i].value > "0") {
-                address += input[i].className += input[i].value; 
+            if (input[i].value > "0" && input[i].type != "text" && input[i].name != "Zustellung") {
+                link += input[i].name + "=" +  " " + input[i].value + "&";
             }
+
+            if (input[i].value != "" && input[i].type == "text" && input[i].name != "Zustellung") {
+                link += input[i].name + "=" +  " " + input[i].value + "&";
+            }
+
+            if (input[i].id == "radio3" && input[i].checked == true) {
+                link += input[i].name + "=" +  " " + "Lieferung" + "&";
+            }
+
+            if (input[i].id == "radio4" && input[i].checked == true) {
+                link += input[i].name + "=" +  " " + "Abholung" + "&";
+            }
+            
         }
         let xhr: XMLHttpRequest = new XMLHttpRequest();
-        xhr.open("GET", address, true);
+        xhr.open("GET", address + link, true);
         xhr.addEventListener("readystatechange", handleStateChange);
         xhr.send();
     }
@@ -29,7 +43,19 @@ namespace L07_SendData {
     function handleStateChange(_event: ProgressEvent): void {
         let xhr: XMLHttpRequest = <XMLHttpRequest>_event.target;
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            document.getElementById("anhang").innerHTML= xhr.response;
+            let prodElement = document.createElement('div');
+            prodElement.innerHTML = `<div id="anhang">
+            </div>`
+            document.getElementById("body").appendChild(prodElement);
+
+
+            let bestellungen: HTMLElement = document.createElement("p");
+            let uberschrift: HTMLElement = document.createElement("h2");
+            uberschrift.innerHTML = "Wir haben Ihre Bestellung erhalten!";
+            document.getElementById("anhang").appendChild(uberschrift);
+            bestellungen.innerHTML = `${xhr.response}`;
+            document.getElementById("anhang").appendChild(bestellungen);
+            console.log(xhr.response);
         }
     }
 } 
