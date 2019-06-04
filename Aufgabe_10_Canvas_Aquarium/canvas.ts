260cf6b9907b3e3974f4f3988f371ebcb2c44dc9
HannesSchwabe <hannes.schwabe@hs-furtwangen.de>
2019-06-04 14:23:35 +0200

    update code

--- Aufgabe_10_Canvas_Aquarium/canvas.ts
+++ Aufgabe_10_Canvas_Aquarium/canvas.ts
@@ -1,209 +1,101 @@
-document.addEventListener("DOMContentLoaded", init);
+namespace L10 {
+    document.addEventListener("DOMContentLoaded", init);
+    let crc: CanvasRenderingContext2D;
+    let canvas: HTMLCanvasElement;
 
-//Globale Variablen
+    let x: number;
+    let y: number;
 
-let canvas: HTMLCanvasElement;
-let crc: CanvasRenderingContext2D;
-
-function init(): void {
-
-    canvas = document.getElementsByTagName("canvas")[0];
-    crc = canvas.getContext("2d");
-
-    let x: number = 10;
-    let y: number = 20;
-    background(x, y);
-
-    for (let i: number = 0; i < 10; i++) {
-        let x: number = Math.random() * canvas.width;
-        //let y: number = Math.random() * canvas.height;
-        plant(x, y);
+    function init(): void {
+        canvas = document.getElementsByTagName("canvas")[0];
+        crc = canvas.getContext("2d");
+        drawbackground(x,y);
+        drawsand(x,y);
+        for (let i: number = 0; i < 10; i++) {
+            let x: number = Math.random() * canvas.width;
+            let y: number = Math.random() * canvas.height;
+            drawfish(x, y);
         }
+        for (let i: number = 0; i<2; i++) {
+            let x: number = Math.random() * canvas.width;
+            let y: number =canvas.height;
+            drawstone(x, y);
+        }
+        for (let i: number = 0; i<100; i++) {
+            let x: number = Math.random() * canvas.width;
+            let y: number = Math.random() * canvas.height;
+            drawbubbles(x, y);
+        }
+        for (let i: number = 0; i<5; i++) {
+            let x: number = Math.random() * canvas.width;
+            let y: number = Math.random() * canvas.height;
+            drawplants(x, y);
+        }
+    }
+
+    function drawbackground(_x: number, _y: number): void {
+        let water: Path2D = new Path2D();
+        water.rect(0,0, 1000, 1000);
+        crc.fillStyle= "blue";
+        crc.fill(water);
+        crc.stroke(water);
+    }
+
+    function drawsand(_x: number, _y: number): void {
+        let sand: Path2D = new Path2D();
+        sand.rect(0, 300, 1000, 500);
+        crc.fillStyle= "yellow";
+        crc.fill(sand);
+        crc.stroke(sand);
+    }
+
+    function drawfish(_x: number, _y: number): void {
+        let fish: Path2D = new Path2D();
+        fish.arc(_x + 200, _y + 200, 30, 0, 360);
+        crc.fillStyle = "orange";
+        crc.fill(fish);
+        crc.stroke(fish);
+
+        let fish1: Path2D = new Path2D();
+        fish1.moveTo(_x + 230, _y + 200);
+        fish1.lineTo(_x + 270, _y + 240);
+        fish1.lineTo(_x + 270, _y + 160);
+        fish1.closePath();
+        crc.fillStyle = "orange";
+        crc.fill(fish1);
+        crc.stroke(fish1);
+
+        let fishmachtauge: Path2D = new Path2D();
+        fishmachtauge.arc(_x + 190, _y + 190, 5, 0, 360);
+        crc.fillStyle = "white";
+        crc.fill(fishmachtauge);
+        crc.stroke(fishmachtauge);
+    }
+
+    function drawstone(_x: number, _y: number): void {
+        let stone: Path2D = new Path2D;
+        stone.moveTo(_x,_y);
+        stone.bezierCurveTo(_x + 150, _y - 100, _x + 200, _y - 120, _x + 400, _y);
+        crc.fillStyle = "grey";
+        crc.fill(stone);
+        crc.stroke(stone);
+    }
+
+    function drawbubbles(_x: number, _y: number): void {
+        let bubble: Path2D = new Path2D();
+        bubble.arc(_x , _y , 3, 0, 360);
+        crc.strokeStyle = "white";
+        crc.stroke(bubble);
+    }
+
+    function drawplants(_x: number, _y: number): void {
+        let plant: Path2D = new Path2D;
+        plant.moveTo(_x + 100, _y + 400);
+        plant.bezierCurveTo(_x + 200, _y + 200, _x + 50, _y + 100, _x + 100, _y + 50);
+        crc.fillStyle = "green";
+        crc.fill(plant);
+        crc.stroke(plant); 
+    }
+   
     
-    for (let i: number = 0; i < 8; i++) {
-        let x: number = Math.random() * canvas.width;
-        let y: number = Math.random() * canvas.height;
-        drawfish(x, y); }
-
-    for (let i: number = 0; i < 5; i++) {
-        let x: number = Math.random() * canvas.width;
-        let y: number = Math.random() * canvas.height;
-        drawbluefish(x, y); }
-    
-    for (let i: number = 0; i < 33; i++) {
-        let x: number = Math.random() * canvas.width;
-        let y: number = Math.random() * canvas.height;
-        drawbubbles(x, y);}
-
-    for (let i: number = 0; i<3; i++) {
-        let x: number = Math.random() * canvas.width;
-        let y: number = Math.random() * canvas.height;
-        stone(x, y);}
-
-}
-
-//function drawfish(_x: number, _y: number): void {
-    //let fishbody: Path2D = new Path2D();
-    //fishbody.moveTo(_x + 60, _y - 30);
-    //fishbody.rect(_x + 20, _y, 60, 30);
-
-    //crc.fillStyle = "red";
-    //crc.fill(fishbody);
-
-    //crc.strokeStyle = "white";
-    //crc.stroke(fishbody);
-
-    //let fishhead: Path2D = new Path2D();
-
-    //fishhead.moveTo(_x + 100, _y + 100);
-    //fishhead.lineTo(_x + 100, _y + 100);
-    //fishhead.lineTo(_x + 100, _y + 100);
-
-    //crc.fillStyle = "blue";
-    //crc.fill(fishhead);
-
-    //crc.strokeStyle = "white";
-    //crc.stroke(fishhead);
-
-    //let flosse1: Path2D = new Path2D();
-
-    //flosse1.moveTo(_x + 13, _y - 1);
-    //flosse1.lineTo(_x + 25, _y);
-    //flosse1.lineTo(_x + 15, _y - 10);
-
-    //crc.fillStyle = "red";
-    //crc.fill(flosse1);
-
-    //crc.strokeStyle = "white";
-    //crc.stroke(flosse1);
-
-    //let flosse2: Path2D = new Path2D();
-
-    //flosse2.moveTo(_x + 5, _y + 5);
-    //flosse2.lineTo(_x + 10, _y +  10);
-    //flosse2.lineTo(_x + 5, _y + 15);
-
-    //crc.fillStyle = "red";
-    //crc.fill(flosse2);
-
-    //crc.strokeStyle = "white";
-    //crc.stroke(flosse2);
-
-    //let orangefisheye: Path2D = new Path2D();
-
-    //orangefisheye.arc(_x + 28 , _y + 8 , 4, 0, 360);
-
-    //crc.fillStyle = "white";
-    //crc.fill(orangefisheye);
-
-    //let orangefisheyepupil: Path2D = new Path2D();
-
-    //orangefisheyepupil.arc(_x + 28 , _y + 8 , 2, 0, 360);
-
-    //crc.fillStyle = "black";
-    //crc.fill(orangefisheyepupil);
-
-//}
-
-function drawbubbles(_x: number, _y: number): void {
-    let bubble: Path2D = new Path2D();
-    bubble.arc(_x + 10, _y, 20, 0, 360);
-    //crc.fillStyle = "lightblue";
-    //crc.fill(bubble);
-
-    crc.strokeStyle = "white";
-    crc.stroke(bubble);
-
-}
-
-function background(_x: number, _y: number): void {
-    let background: Path2D = new Path2D();
-    background.moveTo(_x - 10, _y - 20);
-    background.rect(_x - 10, _y - 20, 600, 400);
-
-    crc.fillStyle = "blue";
-    crc.fill(background);
-
-    let ground: Path2D = new Path2D();
-    ground.moveTo(_x + 10, _y + 300);
-    ground.rect(_x - 10, _y + 250, 600, 400);
-
-    crc.fillStyle = "yellow";
-    crc.fill(ground);
-
-
-}
-
-function stone (_x: number, _y: number): void {
-    let stone: Path2D = new Path2D();
-    stone.moveTo(_x , _y );
-    stone.bezierCurveTo(_x , _y + 50, _x + 700, _y + 250, _x + 250, _y + 400);
-
-    crc.fillStyle = "grey";
-    crc.fill(stone);
-}
-
-function plant(_x: number, _y: number): void {
-    let plant1: Path2D = new Path2D();
-    plant1.moveTo(_x + 100, _y + 350);
-    plant1.bezierCurveTo(_x + 50, _y + 300, _x + 150, _y + 200, _x + 100, _y + 170);
-
-    crc.fillStyle = "green";
-    crc.fill(plant1);
-
-    let stone1: Path2D = new Path2D();
-    stone1.arc(_x + 10 , _y + 360, 10, 0, 360);
-
-    crc.fillStyle = "DarkGrey";
-    crc.fill(stone1);
-
-    let stone2: Path2D = new Path2D();
-    stone2.arc(_x + 30 , _y + 10 + 360, 10, 0, 360);
-
-    crc.fillStyle = "DarkSlateGrey ";
-    crc.fill(stone2);
-}
-
-function drawbluefish(_x: number, _y: number): void {
-
-    let bluefishbody: Path2D = new Path2D();
-
-    bluefishbody.arc(_x + 10, _y, 15, 0, 360);
-
-    crc.fillStyle = "SteelBlue";
-    crc.fill(bluefishbody);
-
-    crc.strokeStyle = "white";
-    crc.stroke(bluefishbody);
-
-    let bluefishfloss: Path2D = new Path2D();
-
-    bluefishfloss.arc(_x , _y + 8, 5, 0, Math.PI);
-
-    crc.fillStyle = "SteelBlue";
-    crc.fill(bluefishfloss);
-
-    let bluefishfloss1: Path2D = new Path2D();
-
-    bluefishfloss1.arc(_x - 5 , _y - 3 , 10, 0, Math.PI);
-
-    crc.fillStyle = "SteelBlue";
-    crc.fill(bluefishfloss1);
-
-    //crc.strokeStyle = "white";
-    //crc.stroke(bluefishfloss);
-
-    let bluefisheye: Path2D = new Path2D();
-
-    bluefisheye.arc(_x + 15 , _y , 4, 0, 360);
-
-    crc.fillStyle = "white";
-    crc.fill(bluefisheye);
-
-    let bluefisheyepupil: Path2D = new Path2D();
-
-    bluefisheyepupil.arc(_x + 15 , _y , 2, 0, 360);
-
-    crc.fillStyle = "black";
-    crc.fill(bluefisheyepupil);
 }
